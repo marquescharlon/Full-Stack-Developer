917f5e9e6a3bf08533c9308a46f630cb0621edfd
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-03-31 23:31:09 -0300

    Injeção de Dependência - Configurando uma injeção de dependência (41% Completo)

--- AppModelo/src/DevIO.UI.Site/Startup.cs
+++ AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -1,3 +1,4 @@
+using DevIO.UI.Site.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -27,6 +28,8 @@ namespace DevIO.UI.Site
             });
           
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
+
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

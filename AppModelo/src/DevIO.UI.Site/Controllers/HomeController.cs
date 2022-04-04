53f29ccd631cd356e2d0348ac4d1c227b5354cdf
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-04-04 13:48:09 -0300

    Injeção de Dependência - Injetando dependências no MVC

--- AppModelo/src/DevIO.UI.Site/Controllers/HomeController.cs
+++ AppModelo/src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPedidoRepository _pedidoRespository;
+        public HomeController(IPedidoRepository pedidoRespository )
+        {
+            _pedidoRespository = pedidoRespository;
+
+        }
+
         public IActionResult Index()
         {
+
+            var pedido = _pedidoRespository.ObterPedido();
+
             return View();
         }
     }

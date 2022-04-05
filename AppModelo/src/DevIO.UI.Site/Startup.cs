c4b9328fed97adbbe45b9e1f86980ccd3576a7e8
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-04-05 14:52:28 -0300

    Entity Framework Core - Configurando o DbContext (49% Completo)

--- AppModelo/src/DevIO.UI.Site/Startup.cs
+++ AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -16,6 +18,14 @@ namespace DevIO.UI.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -26,6 +36,9 @@ namespace DevIO.UI.Site
                 options.AreaViewLocationFormats.Add(item: "/Modulos/{2}/Views/Shared/{0}.cshtml");
                 options.AreaViewLocationFormats.Add(item: "/Views/Shared/{0}.cshtml");
             });
+
+            services.AddDbContext<MeuDbContext>(optionsAction: options =>
+                options.UseSqlServer(Configuration.GetConnectionString(name: "MeuDbContext")));
           
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
 

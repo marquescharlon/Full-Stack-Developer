0e1e3eaf4d12139d332c6e97093046aaede09ef1
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-04-16 18:37:50 -0300

    Configurações - Trabalhando na classe Startup.cs (63% Completo)

--- AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -1,18 +1,9 @@
-using AspNetCoreIdentity.Areas.Identity.Data;
-using AspNetCoreIdentity.Extensions;
-using Microsoft.AspNetCore.Authorization;
+using AspNetCoreIdentity.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreIdentity
 {
@@ -25,32 +16,17 @@ namespace AspNetCoreIdentity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<AspNetCoreIdentityContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddDefaultUI()
-                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
-                options.AddPolicy("PodeLer", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeLer")));
-                options.AddPolicy("PodeEscrever", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeEscrever")));
-            });
-
-            services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHandler>();
+            services.AddIdentityConfig(Configuration);
+            services.AddAuthorizationConfig();
+            services.ResolveDependencies();
 
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -60,7 +36,6 @@ namespace AspNetCoreIdentity
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -71,17 +46,12 @@ namespace AspNetCoreIdentity
             app.UseAuthentication();
             app.UseAuthorization();
 
-            
-
-
             app.UseEndpoints(endpoints =>
             {
 
                 endpoints.MapRazorPages();
 
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

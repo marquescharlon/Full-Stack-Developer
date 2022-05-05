d8f5aadf2e4fcabb19534985efc49826c55b87ff
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-05-05 14:33:39 -0300

    Desenvolvendo aplicação completa, usando .HasColumnName (73% Completo)

---
+++ MinhaAppMvcCompleta/src/DevIO.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,23 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired();
+                
+        }
+    }
+}

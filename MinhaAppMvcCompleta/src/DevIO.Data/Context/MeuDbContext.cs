7e6c1b3001ba45718535c86ddf04bff17124c9cb
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-05-09 14:59:09 -0300

    Problemas ao utilizar a classe MeuDbContext na class startup.cs (73% Completo)

--- MinhaAppMvcCompleta/src/DevIO.Data/Context/MeuDbContext.cs
+++ MinhaAppMvcCompleta/src/DevIO.Data/Context/MeuDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DevIO.Business.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DevIO.Data.Context
 {
@@ -13,5 +14,20 @@ namespace DevIO.Data.Context
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }

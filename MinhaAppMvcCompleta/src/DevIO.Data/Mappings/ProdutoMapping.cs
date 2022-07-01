ade0d5a36a14cbda1cc3980705843f0fdb85c9d2
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-07-01 15:04:13 -0300

    Criando a class EnderecoMapping

--- MinhaAppMvcCompleta/src/DevIO.Data/Mappings/ProdutoMapping.cs
+++ MinhaAppMvcCompleta/src/DevIO.Data/Mappings/ProdutoMapping.cs
@@ -56,4 +56,43 @@ namespace DevIO.Data.Mappings
             builder.ToTable("Fornecedores");
         }
     }
+
+    public class EnderecoMapping : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Logradouro)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(e => e.Numero)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(e => e.Cep)
+                .IsRequired()
+                .HasColumnType("varchar(8)");
+
+            builder.Property(e => e.Complemento)
+                .HasColumnType("varchar(250)");
+
+            builder.Property(e => e.Bairro)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(e => e.Cidade)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(e => e.Estado)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.ToTable("Enderecos");
+        }
+    }
+
+
 }

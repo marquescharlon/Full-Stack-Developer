97f5cd373764d532754151f89b698c55607de92d
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-07-02 19:01:07 -0300

    Camada de Negócio finalizada

---
+++ MinhaAppMvcCompleta/src/DevIO.Business/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    // TEntity foi um nome qualquer
+    // IDisposable é para obrigar o repositória fazer o disposable, liberado a memória
+    // Ele também será específico, por isso, utilizamos o "where"
+    internal interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid Id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid Id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+
+    }
+}

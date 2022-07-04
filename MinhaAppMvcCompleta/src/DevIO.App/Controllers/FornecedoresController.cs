b85c999b5dfd5af8b688c70846ae44ec86cfa47f
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-07-04 13:28:38 -0300

    FornecedorController.cs configurada

--- MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedoresController.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Controllers/FornecedoresController.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using DevIO.App.Data;
 using DevIO.App.ViewModels;
 using DevIO.Business.Interfaces;
 using AutoMapper;
@@ -81,19 +77,17 @@ namespace DevIO.App.Controllers
 
             if (!ModelState.IsValid) return View(fornecedorViewModel);
 
-            return RedirectToAction(nameof(Index));}
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            await _fornecedorRepository.Atualizar(fornecedor);
+
+            return RedirectToAction("Index");
             
         }
 
-        public async Task<IActionResult> Delete(Guid? id)
+        public async Task<IActionResult> Delete(Guid id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var fornecedorViewModel = await _context.FornecedorViewModel
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
             if (fornecedorViewModel == null)
             {
                 return NotFound();
@@ -106,15 +100,11 @@ namespace DevIO.App.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            var fornecedorViewModel = await _context.FornecedorViewModel.FindAsync(id);
-            _context.FornecedorViewModel.Remove(fornecedorViewModel);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool FornecedorViewModelExists(Guid id)
-        {
-            return _context.FornecedorViewModel.Any(e => e.Id == id);
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+            if (fornecedorViewModel == null) return NotFound();
+            await _fornecedorRepository.Remover(id);
+            
+            return RedirectToAction("Index");
         }
 
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)

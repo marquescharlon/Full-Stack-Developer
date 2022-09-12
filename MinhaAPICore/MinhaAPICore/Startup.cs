2939d989c46b40fec18bebf77c3a7ef05603ccd9
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-09-11 23:00:56 -0300

    Implementação da Startup.cs concluída

--- MinhaAPICore/MinhaAPICore/Startup.cs
+++ MinhaAPICore/MinhaAPICore/Startup.cs
@@ -4,19 +4,34 @@
     {
         public Startup(IConfiguration configuration)
         {
-
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
 
         }
 
         public void Configure(WebApplication app, IWebHostEnvironment environment)
         {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
         }
     }
 }
